20350f36167613531769835f9935d10d7a544ccf
Rekha <rekharajan@xminds.com>
2023-09-20 18:17:10 +0530

    update code

---
+++ routes/api.route.js
@@ -0,0 +1,84 @@
+const { PrismaClient } = require('@prisma/client');
+
+const router = require('express').Router();
+const prisma = new PrismaClient;
+
+router.get('/products', async (req, res, next) => {
+  try {
+    const products = await prisma.product.findMany({
+      include: {
+        category: true
+      }
+    });
+    const categories = await prisma.category.findMany({
+      // include: {
+      //   products: true
+      // }
+    });
+    res.json({products, categories});
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/products/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const product = await prisma.product.findUnique({
+      where: {
+        id: Number(id),
+      },
+      include: {
+        category: true
+      },
+    })
+    res.json({product});
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/products', async (req, res, next) => {
+  try {
+      const product = await prisma.product.create({
+        data : req.body,
+      })
+      res.json(product)
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/products/:id', async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const deletedProduct = await prisma.product.delete({
+        where: {
+          id: Number(id),
+        },
+      })
+      res.json(deletedProduct);
+    } catch (error) {
+      next(error);
+    }
+});
+
+router.patch('/products/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const Product = await prisma.product.update({
+      where: {
+        id: Number(id),
+      },
+      data: req.body,
+      include: {
+        category:true
+      }
+    })
+    res.json(Product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
